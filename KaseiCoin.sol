9d2fb2f7d5e859b56b07fe5187bb332fd43781ea
ArianZehtabJ <95341711+ArianZehtabJ@users.noreply.github.com>
2022-05-27 13:16:19 +1000

    Add files via upload

---
+++ KaseiCoin.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+//  Import the following contracts from the OpenZeppelin library:
+//    * `ERC20`
+//    * `ERC20Detailed`
+//    * `ERC20Mintable`
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+
+// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+   /* address payable owner;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You do not have permission to mint these tokens!");
+        _;
+    }
+
+    constructor (uint initial_supply) ERC20Detailed("KaseiCoin", "KSC", 18) public {
+        owner = msg.sender;
+        _mint(owner, initial_supply);
+    }
+
+    function mint(address recipient, uint amount) public onlyOwner {
+        _mint(recipient, amount);
+    }
+}*/
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint initial_supply
+    )
+        ERC20Detailed(name, symbol, 18)
+        public
+    {
+        mint(msg.sender, initial_supply);
+    }
+}
